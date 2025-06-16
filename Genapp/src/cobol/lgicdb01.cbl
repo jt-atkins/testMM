6ba23d1557daa2df1cbf154d4be17d94d9f406e6
Prince111j <138594017+Prince111j@users.noreply.github.com>
2025-06-16 22:58:57 +0100

    Update lgicdb01.cbl

--- Genapp/src/cobol/lgicdb01.cbl
+++ Genapp/src/cobol/lgicdb01.cbl
@@ -17,7 +17,7 @@
       *                                                                *
       * Select customer details from DB2 table                         *
       *                                                                *
-      * TEST: 032                                                      *
+      * TEST: 033                                                      *
       *                                                                *
       ******************************************************************
        IDENTIFICATION DIVISION.
